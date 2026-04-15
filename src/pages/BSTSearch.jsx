f15b0456f063c91f9337d79d923098ff93b4c20f
Annie <w1892798@my.westminster.ac.uk>
2026-04-16 00:26:50 +0100

    chore(bst): fix BST search import

--- src/pages/BSTSearch.jsx
+++ src/pages/BSTSearch.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import AlgorithmLayout from "../components/AlgorithmLayout";
 import BSTRenderer from "../features/bst/traversal/BSTRenderer";
 import { buildBST, buildBSTFromString, presetTrees, defaultValues } from "../features/bst/data/trees";
-import { generateBFSSteps } from "../features/bst/search/bst/search/logic/bstSearchSteps";
+import { generateBSTSearchSteps } from "../../features/bst/search/logic/bstSearchSteps";
 
 // ui icons for playback controls
 import playIcon from "../assets/icons/play.png";
