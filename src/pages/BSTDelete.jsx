85cd16a90f19d2a48026dbb5459c52c2045962f5
Annie <w1892798@my.westminster.ac.uk>
2026-04-16 00:58:47 +0100

    fix(bst delete): add flattenTree in import

--- src/pages/BSTDelete.jsx
+++ src/pages/BSTDelete.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import AlgorithmLayout from "../components/AlgorithmLayout";
 import BSTRenderer from "../features/bst/BSTRenderer";
-import { buildBST, buildBSTFromString, presetTrees, defaultValues } from "../features/bst/data/trees";
+import { buildBST, buildBSTFromString, flattenTree, presetTrees, defaultValues } from "../features/bst/data/trees";
 import { generateBSTDeleteSteps } from "../features/bst/delete/logic/bstDeleteSteps";
 
 // ui icons for playback controls
